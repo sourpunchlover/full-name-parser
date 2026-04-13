59c23306715ea414a4a1eb475d53e1b53104bc18
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-13 14:54:58 -0400

    Added name.split(Pattern.quote) to split name. Also added system.ou.println(names[]) to split each part of the name individually.

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class FullNameParser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! Welcome To The Full Name Parser! Let's Get To Parsing!");
+        System.out.println("Please enter name as 'first last' or 'first middle last'.");
+
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+
+        String[] names = name.split(Pattern.quote( " "));
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+
+
+
+    }
 }
